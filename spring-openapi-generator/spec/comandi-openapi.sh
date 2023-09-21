71064b350edd7bda39121413be5de8d204d2ff35
Mattia Tiburzi <mattia@MBP-di-Mattia.lan>
2023-09-22 00:18:55 +0200

    first commit

---
+++ spring-openapi-generator/spec/comandi-openapi.sh
@@ -0,0 +1,2 @@
+#comandi openapi 
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/spec/petstore.yaml  -g spring -o /local/ -c /local/spec/conf.json
---
+++ springboot-openapi-example/src/main/java/com/example/springbootopenapiexample/SpringbootOpenapiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenapiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenapiExampleApplication.class, args);
+	}
+
+}
